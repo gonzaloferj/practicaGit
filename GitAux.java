ce734d7ac216deb4fd431e564575aa6069bd5cfd
Gonzalo Fernandez Jaime <jofera@alu.uma.es>
2013-03-13 20:57:50 +0100

    modificaciones alumno 1

--- GitAux.java
+++ GitAux.java
@@ -14,6 +14,7 @@ public class GitAux {
 
     public void metodoAlumno1(){
         System.out.println("metodo 1");
+ 	System.out.println("modificado por alum 1");
     }
     
     public void metodoAlumno2(){
@@ -22,5 +23,7 @@ public class GitAux {
     
     public void metodoComunitario(){
         System.out.println("Aqui escribimos todos");
+    	//modificaciones alumno 1
+	System.out.println("Alumno 1 inserta esta sentencia");
     }
 }
